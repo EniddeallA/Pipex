0b31b42b7bca30ae7ddf0e733e06634803ba7aa4
Eniddealla <allaeddinekhalid@gmail.com>
2021-06-13 05:18:43 +0100

    progress

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: eniddealla <eniddealla@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 09:15:48 by akhalid           #+#    #+#             */
-/*   Updated: 2021/06/13 03:59:53 by eniddealla       ###   ########.fr       */
+/*   Updated: 2021/06/13 05:17:07 by eniddealla       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,30 @@
 
 void execute_cmd1(t_pipex *p)
 {
-    
+    printf("child1\n");
+    dup2(p->fd1, 0);
+    dup2(p->fd[1], 1);
+    close(p->fd[0]);
+    execve(p->cmd1, p->cmd1_args, p->env);
+    error_handler("Something went wrong executing cmd1.");
 }
 
 void execute_cmd2(t_pipex *p)
 {
-    
+    printf("child2\n");
+    dup2(p->fd2, 1);
+    dup2(p->fd[0], 0);
+    close(p->fd[1]);
+    execve(p->cmd2, p->cmd2_args, p->env);
+    error_handler("Something went wrong executing cmd2.");
 }
 
 void    pipex(t_pipex *p)
 {
     int child1;
     int child2;
-    int *child1_status;
-    int *child2_status;
+    int child1_status;
+    int child2_status;
 
     if ((child1 = fork()) == -1)
         error_handler("Something went wrong while creating child1");
@@ -42,13 +52,8 @@ void    pipex(t_pipex *p)
         if ((child2 = fork()) == -1)
             error_handler("Something went wrong creating child2");
     }
-    else if(child2 == 0)
-        execute_cmd2(p);
     else
-    {
-        wait_pid(child1, &child1_status, WCONTINUED);
-        wait_pid(child2, &child2_status, WCONTINUED);
-    }
+        execute_cmd2(p);
 }
 
 void getpath(t_pipex **p, int cmd_n)
@@ -63,8 +68,6 @@ void getpath(t_pipex **p, int cmd_n)
     check = 0;
     while((*p)->env[i])
     {
-        if (i == 91)
-            printf("k");
         if (!ft_strncmp("PATH", (*p)->env[i], 4))
         {
             new_path = (char *)malloc(sizeof(char) * ft_strlen(ft_strrchr((*p)->env[i], '=')));
@@ -85,8 +88,8 @@ void getpath(t_pipex **p, int cmd_n)
             {
                 (*p)->cmd1 = ft_strdup(cmd);
                 free(cmd);
-                break;
                 check = 1;
+                break;
             }
             free(cmd);
         }
@@ -98,8 +101,8 @@ void getpath(t_pipex **p, int cmd_n)
             {
                 (*p)->cmd2 = ft_strdup(cmd);
                 free(cmd);
-                break;
                 check = 1;
+                break;
             }
             free(cmd);
         }
@@ -132,8 +135,8 @@ int     main(int argc, char *argv[], char **env)
     if (argc == 5)
     {
         p = (t_pipex *)malloc(sizeof(t_pipex));
-        if (pipe(p->fd) == - 1)
-            error_handler("Something went wrong using pipe()");
+        if (pipe(p->fd) == -1)
+            error_handler("Something went wrong using Pipe.");
         p->fd1 = open(argv[1], O_RDONLY);
         p->fd2 = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
         p->env = env;
@@ -141,8 +144,11 @@ int     main(int argc, char *argv[], char **env)
         p->cmd2 = argv[3];
         check_pipex(&p);
         pipex(p);
+        close(p->fd1);
+        close(p->fd2);
     }
     else
         error_handler("USAGE: '> ./pipex file1 cmd1 cmd2 file2'.");
+    
     return (0);
 }
